9edbd6741615bdee9ad7cd49480c9434a8388c5b
RiderxL <55648819+RiderxL@users.noreply.github.com>
2020-02-14 01:52:09 +0800

    Update Comp2.vb

--- Comp2.vb
+++ Comp2.vb
@@ -1,28 +1,28 @@
 Public Class Form1
     Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
-        Dim array(3) As String
-        array(0) = "Harry Potter"
-        array(1) = "Florante at Laura"
-        array(2) = "Visual Basic Programming"
-        array(3) = "Noli Me Tangere"
+        Dim books(3) As String
+        books(0) = "Harry Potter"
+        books(1) = "Florante at Laura"
+        books(2) = "Visual Basic Programming"
+        books(3) = "Noli Me Tangere"
 
         If ComboBox1.SelectedIndex = 0 Then
-            For Each book As String In array
+            For Each book As String In books
                 ListBox1.Items.Add(book)
             Next
 
         ElseIf ComboBox1.SelectedIndex = 1 Then
             ListBox1.Items.Clear()
-            ListBox1.Items.Add(array(0))
+            ListBox1.Items.Add(books(0))
 
         ElseIf ComboBox1.SelectedIndex = 2 Then
             ListBox1.Items.Clear()
-            ListBox1.Items.Add(array(2))
+            ListBox1.Items.Add(books(2))
 
         ElseIf ComboBox1.SelectedIndex = 3 Then
             ListBox1.Items.Clear()
-            ListBox1.Items.Add(array(1))
-            ListBox1.Items.Add(array(3))
+            ListBox1.Items.Add(books(1))
+            ListBox1.Items.Add(books(3))
         End If
     End Sub
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
